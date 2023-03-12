838881fbbbdcf5e270dfe7b5760f2184ace2903d
Md Sajib Pramanic <118989261+SAJIB3489@users.noreply.github.com>
2023-03-12 16:28:31 +0200

    Add files via upload

---
+++ If Squre root.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>  
+#include<math.h>
+
+int main(int  argc, char** argv) {
+
+	double number;
+	printf("Enter a number:\n");
+	scanf("%lf", &number);
+	if (number > 0) {
+
+		printf("Square root of %lf is %lf\n", number, sqrt(number));
+
+	}
+	else {
+		printf("Error!!\n\n");
+	}
+}
